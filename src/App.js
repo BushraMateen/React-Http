a834569c24205e70b7600052b73e49289066cfd3
mastanpatel <Bushra.mateen95@outlook.com>
2021-11-30 22:25:02 +0530

    Postr request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <PostList />
+      <PostForm />
+      {/* <PostList /> */}
     </div>
   );
 }
