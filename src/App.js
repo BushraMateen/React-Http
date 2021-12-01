f64f3e39110350e361a7cffc22ae8d5114ae94b7
mastanpatel <Bushra.mateen95@outlook.com>
2021-12-01 22:19:49 +0530

    arrays

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import ClassCounter from './Components/ClassCounter';
 import HookCounter from './Components/HookCounter';
 import HookCounter2 from './Components/HookCounter2';
 import HookCounter3 from './Components/HookCounter3';
+import HookCounter4 from './Components/HookCounter4';
 import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3 />
+      <HookCounter4 />
+      {/* <HookCounter3 /> */}
       {/* <HookCounter2 /> */}
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
