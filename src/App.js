7487a857e832b6653bbeea7d86f8100889d34653
mastanpatel <Bushra.mateen95@outlook.com>
2021-12-01 21:24:53 +0530

    hookcounter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 
 import './App.css';
+import ClassCounter from './Components/ClassCounter';
+import HookCounter from './Components/HookCounter';
+import HookCounter2 from './Components/HookCounter2';
 import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <PostForm />
+      <HookCounter2 />
+      {/* <HookCounter /> */}
+      {/* <ClassCounter /> */}
+      {/* <PostForm /> */}
       {/* <PostList /> */}
     </div>
   );
---
+++ src/Components/HookCounter.js
@@ -0,0 +1,13 @@
+import React, {useState} from "react";
+
+function HookCounter2(){
+    const [ count,setCount ] = useState(0)
+     
+    return(
+        <div>
+            <button onClick={()=> setCount(count + 1)}>Count {count}</button>
+        </div>
+    )
+}
+
+export default HookCounter2
