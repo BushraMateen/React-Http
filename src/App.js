0b5653eed37554ee52ae15c471dfe877d2e44664
mastanpatel <Bushra.mateen95@outlook.com>
2021-12-01 21:40:30 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import HookCounter from './Components/HookCounter';
 import HookCounter2 from './Components/HookCounter2';
+import HookCounter3 from './Components/HookCounter3';
 import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 
 function App() {
   return (
     <div className="App">
-      <HookCounter2 />
+      <HookCounter3 />
+      {/* <HookCounter2 /> */}
       {/* <HookCounter /> */}
       {/* <ClassCounter /> */}
       {/* <PostForm /> */}
