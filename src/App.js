5c9685f25edc00ea6f285b30f2377c9ac986d64d
mastanpatel <Bushra.mateen95@outlook.com>
2021-12-03 22:09:49 +0530

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import ClassCounter from './Components/ClassCounter';
+import DataFetching from './Components/DataFetching';
 import HookCounter from './Components/HookCounter';
 import HookCounter2 from './Components/HookCounter2';
 import HookCounter3 from './Components/HookCounter3';
 import HookCounter4 from './Components/HookCounter4';
 import HookCounterOne from './Components/HookCounterone';
 import HookMouse from './Components/HookMouse';
+import IntervalHookCounter from './Components/IntervalHookCounter';
 import MouseContainer from './Components/MouseContainer';
 import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
@@ -14,7 +16,9 @@ import PostList from './Components/PostList';
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <DataFetching />
+      {/* <IntervalHookCounter /> */}
+      {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
       {/* <HookCounterOne /> */}
       {/* <HookCounter4 /> */}
