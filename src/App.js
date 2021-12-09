a5d411512d88c32a1e7fe26996d8639bb76c82bc
mastanpatel <Bushra.mateen95@outlook.com>
2021-12-09 13:05:21 +0530

    usereducer and usecontext

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React,{useReducer} from 'react';
 import './App.css';
 import ClassCounter from './Components/ClassCounter';
 import ComponentC from './Components/ComponentC';
@@ -18,15 +18,40 @@ import PostForm from './Components/PostForm';
 import PostList from './Components/PostList';
 import ConterOne from './Components/ConterOne';
 import ConterTwo from './Components/ConterTwo';
+import ComponentA from './Components/ComponentA';
+import ComponentB from './Components/ComponentB';
+import ComponentC1 from './Components/ComponentC1';
 
 
-export const UserContext  = React.createContext()
-export const ChannelContext = React.createContext()
+ export const UserContext  = React.createContext()
+ export const ChannelContext = React.createContext()
 
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+    switch(action){
+        case 'increment':
+            return state + 1
+        case 'decrement':
+            return state - 1
+         case 'reset':
+            return initialState
+        default :
+        return state
+    }
+}
 function App() {
+  const [count,dispatch] = useReducer(reducer,initialState)
   return (
-    <div className="App">
-      <ConterTwo />
+    <CountContext.Provider 
+        value = {{ countState:count, countDispatch: dispatch}} >
+      <div className="App">
+      Count - {count}
+      <ComponentA />
+      <ComponentB />
+      <ComponentC1 />
+      {/* <ConterTwo /> */}
       {/* <ConterOne /> */}
       {/* <UserContext.Provider value={'Bushra'}>
         <ChannelContext.Provider value = {'Mateen'}>
@@ -45,7 +70,8 @@ function App() {
       {/* <ClassCounter /> */}
       {/* <PostForm /> */}
       {/* <PostList /> */}
-    </div>
+      </div>
+    </CountContext.Provider>
   );
 }
 
---
+++ src/Components/ComponentB.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComponentD1 from './ComponentD1'
+
+
+function ComponentB() {
+    return(
+        <div>
+          <ComponentD1 />
+        </div>
+    )
+}
+
+export default ComponentB
